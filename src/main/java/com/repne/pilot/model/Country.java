5c62515e2e25704b650c0e846c3aefd07f84dc73
Emre Erkmen <emre.erkmen@hotmail.com>
2022-12-03 14:25:39 +0300

    remove un used imports

--- src/main/java/com/repne/pilot/model/Country.java
+++ src/main/java/com/repne/pilot/model/Country.java
@@ -1,10 +1,7 @@
 package com.repne.pilot.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 
 import lombok.Data;
 
